68116816883390451cf8e47c01acc36a42d321cc
SerjK <sergk@gmail.com>
2024-01-20 18:14:01 +0200

    new project

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require("gulp-rename"); // Добавлен импорт
+
+async function server() {
+    browserSync.init({
+        server: {
+            baseDir: "src"
+        }
+    });
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+}
+
+function styles() {
+    return gulp.src("src/sass/**/*.+(scss|sass)")
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(rename({ suffix: '.min', prefix: '' })) // Использование функции rename
+        .pipe(autoprefixer({
+            browsers: ['last 16 versions'],
+            cascade: false
+        }))
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+}
+
+function watch() {
+    gulp.watch("src/sass/**/*.+(scss|sass)", styles);
+}
+
+exports.default = gulp.parallel(watch, server, styles);
