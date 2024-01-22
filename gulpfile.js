fe656e06cda14a43d3f6083472e027f0785f2cf0
SerjK <sergk@gmail.com>
2024-01-22 18:36:20 +0200

    finished with style settings

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
-const autoprefixer = require('gulp-autoprefixer');
+//const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename"); // Добавлен импорт
 
 async function server() {
@@ -15,20 +15,20 @@ async function server() {
 }
 
 function styles() {
-    return gulp.src("src/sass/**/*.+(scss|sass)")
+    return gulp.src("src/css/**/*.+(scss|sass)")
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(rename({ suffix: '.min', prefix: '' })) // Использование функции rename
-        .pipe(autoprefixer({
-            browsers: ['last 16 versions'],
-            cascade: false
-        }))
+        //.pipe(autoprefixer({
+        //    browsers: ['last 16 versions'],
+        //    cascade: false
+        //}))
         .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("src/css/"))
         .pipe(browserSync.stream());
 }
 
 function watch() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", styles);
+    gulp.watch("src/css/*.+(scss|sass)", styles);
 }
 
 exports.default = gulp.parallel(watch, server, styles);
