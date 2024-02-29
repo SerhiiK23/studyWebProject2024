515cfdb710aaa44fed4a7374075da91cba4a230c
SerjK <sergk@gmail.com>
2024-02-29 22:00:43 +0200

    made tab section

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,3 @@
-// $(document).ready(function(){
-//     $('.carousel__inner').slick({
-//         infinite: true,
-//         speed: 300,
-//         slidesToShow: 1,
-//         adaptiveHeight: true,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/left_arrow.png"></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="images/icons/right_arrow.png"></button>',
-//         responsive: [
-//             {
-//                 breakpoint: 992,
-//                 settings: {
-//                     dots: true,
-//                     arrows: false
-//                 }
-//             }
-//         ]
-
-//       });
-//   });
-
 var slider = tns({
     container: '.carousel__inner',
     items: 1,
@@ -26,12 +5,39 @@ var slider = tns({
     autoplay: false,
     controls: false,
     nav: false
-  });
-
+});
+    
 document.querySelector('.next').onclick = function () {
     slider.goTo('next');
 };
-
+    
 document.querySelector('.prev').onclick = function () {
     slider.goTo('prev');
 };
+    
+let elem = document.querySelector('#elem');
+
+(function($) {
+    $(function() {
+      
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+
+      function toggleCard (item){
+        $(item).each(function(i){
+            $(this).on('click', function(e){
+                e.preventDefault();
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            })
+          })
+      }
+
+      toggleCard('.catalog-item__list');
+      toggleCard('.catalog-item__link');
+      
+    });
+    })(jQuery);
